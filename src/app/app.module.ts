b4e5e9e348b4938855e6ffc9ba7c2425dd3237fc
Anton Starodubtsev <astaro@softserveinc.com>
2019-04-08 16:59:46 +0300

    implemented store, effects and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
+import { BorderedNumberComponent } from './bordered-number/bordered-number.component';
+import { strangeReducer } from './store/strange.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorderedNumberComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ data: strangeReducer }),
+    EffectsModule.forRoot([AppEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
